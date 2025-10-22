a73c11e585f4f6ee564c2c45dc9f431ce754a31d
Romain DE BRITO <romain.debrito@gmail.com>
2025-10-22 16:20:27 +0200

    Add worker converter

--- files/profile.d/040-fileconverter.sh
+++ files/profile.d/040-fileconverter.sh
@@ -6,16 +6,12 @@ start_fileconverter() {
 }
 
 ensure_fileconverter() {
-	start_fileconverter
-
-	while true
-	do
-		sleep 15s
-		pidof "converter" > /dev/null \
-			|| {
-				echo "FileConverter does not seem to be running. Respawning." >&2
-				start_fileconverter
-			}
+	while true; do
+		sleep 30s
+		if ! pgrep -f '/app/server/DocService/docservice' >/dev/null; then
+			echo "FileConverter does not seem to be running. Respawning."
+			start_fileconverter
+		fi
 	done &
 }
 
