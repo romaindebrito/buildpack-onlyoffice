98f5fcc40e530a78719696c11ec8a25785f04908
Romain <romain-debrito@Ordinateur-portable-de-Romain.local>
2025-10-22 11:45:53 +0200

    Add new variable to disable daemin file converter

--- files/profile.d/040-fileconverter.sh
+++ files/profile.d/040-fileconverter.sh
@@ -21,7 +21,7 @@ ensure_fileconverter() {
 
 # Only start OnlyOffice fileconverter if the conditions are OK
 # `_OO_START` is computed in 020-onlyoffice.sh
-if [ -z "${ONLYOFFICE_DOCUMENTSERVER_DISABLE_DAEMON}" ] \
+if [ -z "${ONLYOFFICE_FILECONVERTER_DISABLE_DAEMON}" ] \
 		&& [ "${_OO_START}" -eq 0 ]
 then
 	ensure_fileconverter
